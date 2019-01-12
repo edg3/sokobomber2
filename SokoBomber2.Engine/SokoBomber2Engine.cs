3d7fe0d8de51b8b62e789dbce05f54705ebaba40
edg3 <loveland.ernest@gmail.com>
2019-01-12 10:08:40 +0200

    initial engine drawing

--- SokoBomber2.Engine/SokoBomber2Engine.cs
+++ SokoBomber2.Engine/SokoBomber2Engine.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using SokoBomber2.States;
+using System;
+using System.Collections.Generic;
 
 namespace SokoBomber2
 {
     public class SokoBomber2Engine
     {
+        public ISpriteBatch SpriteBatch { get; }
+        public IContentManager Content { get; }
         public SokoBomber2Engine(ISpriteBatch _spriteBatch, IContentManager _content)
         {
+            SpriteBatch = _spriteBatch;
+            Content = _content;
+        }
 
+        private List<IState> States { get; set; }
+        public void AddState(IState _state)
+        {
+            if (States == null) States = new List<IState>();
+
+            _state.Load(Content);
+            States.Add(_state);
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw()
+        {
+            try
+            {
+                States[States.Count - 1].Draw(SpriteBatch);
+            }
+            catch { }
         }
     }
 }
