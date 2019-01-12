f87450310b81fa0da26a9af246d9704f81cae282
edg3 <loveland.ernest@gmail.com>
2019-01-12 13:38:01 +0200

    initial map work

--- SokoBomber2.Engine/States/StatePlay.cs
+++ SokoBomber2.Engine/States/StatePlay.cs
@@ -11,9 +11,20 @@ namespace SokoBomber2.Engine.States
     {
         public void Draw(ISpriteBatch _spriteBatch)
         {
+            for (int x = 0; x < levelWidth; x++)
+            {
+                for (int y = 0; y < levelHeight; y++)
+                {
+                    _spriteBatch.Draw("tGround", x * 32 + 390, y * 32 + 210);
+                }
+            }
+
             _spriteBatch.Draw("mouse", SokoBomber2Engine.Instance.MouseX, SokoBomber2Engine.Instance.MouseY);
         }
 
+        uint[,] levelTiles;
+        uint levelWidth, levelHeight;
+        int playerTileX, playerTileY;
         public void Load(IContentManager _content)
         {
             // Art
@@ -32,6 +43,28 @@ namespace SokoBomber2.Engine.States
             _content.Load("tWall");
 
             // Level
+            Random r = new Random();
+            levelWidth = (uint)(5 + r.Next(10));
+            levelHeight = (uint)(5 + r.Next(10));
+            levelTiles = new uint[levelWidth, levelHeight];
+            for (int x = 0; x < levelWidth; x++)
+            {
+                for (int y = 0; y < levelHeight; y++)
+                {
+                    if ((x == 0) || (x == levelWidth - 1) || (y == 0) || (y == levelHeight - 1))
+                    {
+                        levelTiles[x, y] = 1;
+                    }
+                    else
+                    {
+                        levelTiles[x, y] = 2;
+                    }
+                }
+            }
+            levelTiles[1, 1] = 3;
+            playerTileX = 1;
+            playerTileY = 1;
+            // TODO - enum tile types
         }
         
         public void Update()
