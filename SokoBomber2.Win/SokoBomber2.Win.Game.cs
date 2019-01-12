1642578c94df176cbbea4037ac313f9471520f4a
edg3 <loveland.ernest@gmail.com>
2019-01-12 10:48:57 +0200

    intial input wrap

--- SokoBomber2.Win/SokoBomber2.Win.Game.cs
+++ SokoBomber2.Win/SokoBomber2.Win.Game.cs
@@ -33,11 +33,39 @@ namespace SokoBomber2.Win
 
             SokoBomber2Engine.AddState(new StateMenu());
         }
-        
+
+        KeyboardState PreviousKeyboardState;
+        KeyboardState _currentKeyboardState;
+        KeyboardState CurrentKeyboardState {
+            get {
+                return _currentKeyboardState;
+            }
+            set {
+                PreviousKeyboardState = _currentKeyboardState;
+                _currentKeyboardState = value;
+            }
+        }
+        MouseState PreviousMouseState;
+        MouseState _currentMouseState;
+        MouseState CurrentMouseState
+        {
+            get
+            {
+                return _currentMouseState;
+            }
+            set
+            {
+                PreviousMouseState = _currentMouseState;
+                _currentMouseState = value;
+            }
+        }
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+
+            CurrentKeyboardState = Keyboard.GetState();
+            _currentMouseState = Mouse.GetState();
 
             SokoBomber2Engine.Update();
 
