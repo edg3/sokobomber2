1fcbee7384dca924bbcd659e54031659cb89a5a8
edg3 <loveland.ernest@gmail.com>
2019-01-12 10:36:00 +0200

    menu interface proto in

--- SokoBomber2.Engine/States/StateMenu.cs
+++ SokoBomber2.Engine/States/StateMenu.cs
@@ -11,7 +11,13 @@ namespace SokoBomber2.Engine.States
     {
         public void Draw(ISpriteBatch _spriteBatch)
         {
-            _spriteBatch.Draw("mOptions", 0, 0);
+            _spriteBatch.Draw("mSokobomber", 0, 0);
+            _spriteBatch.Draw("mOptions", 550, 0);
+
+            _spriteBatch.Draw("mButtonPlay", 604, 25);
+            _spriteBatch.Draw("mButtonAbout", 604, 105);
+
+            _spriteBatch.Draw("mTwo", 165, 370);
         }
 
         public void Load(IContentManager _content)
@@ -19,10 +25,11 @@ namespace SokoBomber2.Engine.States
             _content.Load("mOptions");
             _content.Load("mButtonAbout");
             _content.Load("mButtonPlay");
-            _content.Load("mDiamond");
-            _content.Load("mQuit");
             _content.Load("mSokobomber");
             _content.Load("mTwo");
+
+            _content.Load("mDiamond");
+            _content.Load("mQuit");
         }
 
         public void Update()
