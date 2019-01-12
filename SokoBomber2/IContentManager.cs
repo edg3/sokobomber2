d5a6a23f0ff41da7c588e46c5ac6168deaab16b6
edg3 <loveland.ernest@gmail.com>
2019-01-12 08:52:46 +0200

    added interfaces

---
+++ SokoBomber2/IContentManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SokoBomber2
+{
+    public interface IContentManager
+    {
+        void Load(string _name);
+    }
+}
